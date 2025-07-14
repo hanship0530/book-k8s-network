f91b9c92d446b48f74273cdfde6a59e23dc0e1c3
hanship0530 <hanship0530@gmail.com>
2025-07-14 18:51:56 +0900

    change script

--- 5/init_cfg.sh
+++ 5/init_cfg.sh
@@ -53,15 +53,13 @@ echo "[TASK 10] Disable and turn off SWAP"
 swapoff -a
 
 echo "[TASK 11] Install Kubernetes components (kubeadm, kubelet and kubectl) - v$2"
-mkdir -p -m 755 /etc/apt/keyrings
-K8SMMV=$(echo $2 | sed -En 's/^([0-9]+\.[0-9]+)\..*/\1/p')
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v$K8SMMV/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$K8SMMV/deb/ /" >> /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg >/dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update >/dev/null 2>&1
-apt-get install -y kubelet=$2 kubectl=$2 kubeadm=$2 >/dev/null 2>&1
+curl -LO "https://dl.k8s.io/release/$2" >/dev/null 2>&1
+chmod +x kubectl
+mv kubelet /usr/local/bin/
+apt-get install -y kubelet=$2-00 kubeadm=$2-00 >/dev/null 2>&1
 apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 systemctl enable kubelet && systemctl start kubelet
 
